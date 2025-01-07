40612b0427fb494830c59c7fb18557c07ba75f99
JoseBruno2005 <josebrunobispoparnaiba@gmail.com>
2025-01-07 16:53:19 -0300

    Criação do authMiddleware para verificação de token nas rotas

---
+++ Backend/auth/authMiddleware.js
@@ -0,0 +1,20 @@
+import {verifyToken} from "./authService"
+
+function authMiddleware(req, res, next){
+    const token = req.header('Authorization')?.replace('Bearer', '');
+
+    if(!token){
+        return res.status(401).json({error: "Token não fornecido!"});
+    }
+
+    try{
+        const decoded = verifyToken(token);
+        req.user = decoded;
+        next();
+    }catch(error){
+        res.status(401).json({error: "Token inválido!"});
+    }
+
+}
+
+module.exports = authMiddleware;
