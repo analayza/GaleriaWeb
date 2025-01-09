84b2c9e7f3071056523b9f7e77e0941e8b09d0fd
JoseBruno2005 <josebrunobispoparnaiba@gmail.com>
2025-01-09 16:36:13 -0300

    Criando pagina de login

--- Frontend/GaleriaWeb/src/pages/Login.jsx
+++ Frontend/GaleriaWeb/src/pages/Login.jsx
@@ -1,7 +1,42 @@
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import TextInput from '../components/TextInput';
+
 export default function Login(){
     return(
-        <h1>
-            Login
-        </h1>
+        <>
+            <h1>
+                Login
+            </h1>
+            <div>
+                <Formik  initialValues={{
+                    email: "",
+                    password: ""
+                }} validationSchema={Yup.object({
+                    email: Yup.string()
+                    .required("Obrigatório"),
+                    password: Yup.string()
+                    .required("Obrigatório")
+                })} onSubmit={async () => {
+
+                }}>
+                    <Form>
+                        <TextInput
+                            label="Email"
+                            name= "email"
+                            type= "email"
+                            password= "Digite seu email" 
+                        />
+                        <TextInput
+                            label="Senha"
+                            name= "password"
+                            type= "password"
+                            password= "Digite sua senha" 
+                        />
+                        <button type="submit">Login</button>
+                    </Form> 
+                </Formik>
+            </div>
+        </>
     )
 }
